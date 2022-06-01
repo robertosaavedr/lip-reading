4fcd84eebec72d70f7e664731835a716166088cf
robertosaavedr <robertocarlos.saavedrabaylon@gmail.com>
2022-06-01 12:45:05 +0200

    change name of hmm to hmm_words

--- hmm_words.py
+++ hmm_words.py
@@ -4,20 +4,9 @@ import os
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
-from keras.preprocessing import sequence
-from sklearn.svm import SVC
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
-
-from keras.models import Sequential
-from keras.layers import Dense, LSTM
-from keras.layers.embeddings import Embedding
-from keras.preprocessing import sequence
-from keras.layers import SimpleRNN
-from sklearn.utils import resample
-
 from itertools import compress
+from keras.preprocessing import sequence
+
 
 labels = pd.DataFrame(columns=['person' , 'sentence' , 'attemp' , 'frame'])
 in_matrix = None
@@ -29,8 +18,8 @@ for person in os.listdir("lip_reading/words"):
             for frame in sorted(os.listdir("lip_reading/words/%s/%s/%s" % (person, sentence, attemp))):
                 im = cv.imread("lip_reading/words/%s/%s/%s/%s" % (person, sentence, attemp, frame), cv.IMREAD_GRAYSCALE)
                 #sizes.append(im.shape) 
-                im = cv.resize(im, [32, 16], interpolation= cv.INTER_CUBIC)
-                vec = im.reshape(32*16)
+                im = cv.resize(im, [27, 13], interpolation= cv.INTER_CUBIC)
+                vec = im.reshape(27*13)
                 size = np.array([len(os.listdir("lip_reading/words/%s/%s/%s" % (person, sentence, attemp))), person, int(sentence), int(attemp), frame])
                 try:
                     in_matrix = np.vstack((in_matrix, vec))
@@ -74,6 +63,8 @@ for i in order:
     y_train = np.append(y_train, y_train_total[lower, 2])
     lower = cum
 
+seq_transformed = sequence.pad_sequences(X_train_3d)
+X_train_3d = list(seq_transformed)
 
 ## TEST 
 indexes = y_test_total[:, 0].astype(int)
@@ -95,6 +86,9 @@ for i in order:
     y_test = np.append(y_test, y_test_total[lower, 2])
     lower = cum
 
+seq_transformed_test = sequence.pad_sequences(X_test_3d, 27)
+X_test_3d = list(seq_transformed_test)
+
 # validation data
 indexes = y_validation_total[:, 0].astype(int)
 order = []
@@ -115,6 +109,9 @@ for i in order:
     y_validation = np.append(y_validation, y_validation_total[lower, 2])
     lower = cum
 
+seq_transformed_validation = sequence.pad_sequences(X_validation_3d, 27)
+X_validation_3d = list(seq_transformed_validation)
+
 # training with hmm gmm
 
 labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -124,9 +121,8 @@ y_validation = y_validation.astype(int)
 
 hmms = []
 
-
 for i, label in enumerate(labels):
-    hmm = GMMHMM(label = label, n_states=6, n_components=9, topology='left-right')
+    hmm = GMMHMM(label = label, n_states=3, n_components=7, topology='left-right')
     hmm.set_random_initial()
     hmm.set_random_transitions()
     hmm.fit(list(compress(X_train_3d, y_train==label)))
@@ -136,6 +132,6 @@ clf = HMMClassifier()
 clf.fit(hmms)
 # using validation set to tune hyperparameters/topology of hmm
 # predictions = clf.predict(X_validation_3d)
-accuracy, confusion = clf.evaluate(X_validation_3d, y_validation)
-accuracy, confusion = clf.evaluate(X_train_3d, y_train)
-accuracy, confusion = clf.evaluate(X_test_3d, y_test)
+accuracy1, confusion1 = clf.evaluate(X_validation_3d, y_validation)
+accuracy2, confusion2 = clf.evaluate(X_train_3d, y_train)
+accuracy3, confusion3 = clf.evaluate(X_test_3d, y_test)
